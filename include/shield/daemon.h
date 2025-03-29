90b3f316135af304d007c83cfe748cf392788875
xtrm <oss@xtrm.me>
2025-03-29 21:05:28 +0100

    ✨ feature(daemon.h): add client struct
    Signed-off-by: xtrm <oss@xtrm.me>

--- include/shield/daemon.h
+++ include/shield/daemon.h
@@ -6,12 +6,25 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:20:31 by kiroussa          #+#    #+#             */
-/*   Updated: 2025/03/25 15:17:10 by kiroussa         ###   ########.fr       */
+/*   Updated: 2025/03/29 19:24:40 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DAEMON_H
 # define DAEMON_H
+
+enum e_client_state
+{
+	CLIENT_STATE_CONNECTED = 0,
+	CLIENT_STATE_AUTHENTICATED,
+	CLIENT_STATE_SHELL,
+};
+
+typedef struct s_client
+{
+	int					fd;
+	enum e_client_state	state;
+}	t_client;
 
 void	shield_daemonize(void);
 void	shield_daemon_start(void);
