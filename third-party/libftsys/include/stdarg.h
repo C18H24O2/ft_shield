8c6aa1954b2289dfdd5f6fd47b43c5cb6dd71989
xtrm <oss@xtrm.me>
2025-09-29 09:25:57 +0200

    ✨ feature(libftsys): added malo va_list
    Signed-off-by: xtrm <oss@xtrm.me>

--- third-party/libftsys/include/stdarg.h
+++ third-party/libftsys/include/stdarg.h
@@ -6,17 +6,24 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 01:25:17 by kiroussa          #+#    #+#             */
-/*   Updated: 2025/05/11 04:42:50 by kiroussa         ###   ########.fr       */
+/*   Updated: 2025/09/29 09:25:01 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STDARG_H
 # define STDARG_H
 
+# if defined(__builtin_va_list)
 typedef __builtin_va_list	va_list;
 
-# define va_start(v, l) __builtin_va_start(v, l)
-# define va_end(v) __builtin_va_end(v)
-# define va_arg(v, l) __builtin_va_arg(v, l)
+#  define va_start(v, l) __builtin_va_start(v, l)
+#  define va_end(v) __builtin_va_end(v)
+#  define va_arg(v, l) __builtin_va_arg(v, l)
+# else // !__builtin_va_list
+typedef char *	va_list;
+# define va_start(ap, parmn) (void)((ap) = (char*)(&(parmn) + 1))
+# define va_arg(ap, type) (((type*)((ap) = ((ap) + sizeof(type))))[-1])
+# define va_end(ap) (void)((ap) = 0)
+#endif // __builtin_va_list
 
 #endif // STDARG_H
