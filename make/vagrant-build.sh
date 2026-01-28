41ca1289b3a049fc553e73006e55aead78129671
dyn <contact@dynamicdispat.ch>
2026-01-28 14:32:23 +0100

    🐛 fix(make/vagrant-build.sh): oops
    Signed-off-by: dyn <contact@dynamicdispat.ch>

--- make/vagrant-build.sh
+++ make/vagrant-build.sh
@@ -4,7 +4,7 @@ rm -f /vagrant/third-party/woody-woodpacker/third-party/libft/tools/provide-targ
 
 # shield
 make fclean -C /vagrant
-make DEVELOPMENT=1 USE_WW=0 SYS_CC=clang-20 SYS_CXX=clang++-20 -C /vagrant copy-target PROJECT_TYPE=0 TO=/home/vagrant/
+make DEVELOPMENT=0 USE_WW=0 SYS_CC=clang-20 SYS_CXX=clang++-20 -C /vagrant copy-target PROJECT_TYPE=0 TO=/home/vagrant/
 
 # matt
 make oclean -C /vagrant
